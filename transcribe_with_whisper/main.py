7058dd8bd3f682c43d87f0a3b4969cf734d8ce82
Jay Pfaffman <jay@literatecomputing.com>
2025-09-16 15:53:46 -0500

    up to 6 speakers

--- transcribe_with_whisper/main.py
+++ transcribe_with_whisper/main.py
@@ -249,7 +249,7 @@ def main():
     speaker_names = sys.argv[2:]  # any extra args are speaker names
 
     # Default speaker labels
-    default_speakers = ["Speaker 1", "Speaker 2", "Speaker 3"]
+    default_speakers = ["Speaker 1", "Speaker 2", "Speaker 3", "Speaker 4", "Speaker 5", "Speaker 6"]
 
     # If user provides names, override defaults
     for i, name in enumerate(speaker_names):
