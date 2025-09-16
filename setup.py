f0b1bb5ecc991406627ada90069573718e85091b
Jay Pfaffman <jay@literatecomputing.com>
2025-09-16 15:13:23 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="transcribe-with-whisper",
     version="0.1.0",
@@ -9,8 +12,8 @@ setup(
         "webvtt-py",
         "pyannote.audio",
         "huggingface_hub",
-        "torch",            # default CPU version will be installed
-        "faster-whisper",   # will use CPU if torch is CPU-only
+        "torch",
+        "faster-whisper",
     ],
     entry_points={
         "console_scripts": [
@@ -20,7 +23,7 @@ setup(
     python_requires=">=3.8",
     include_package_data=True,
     description="Video transcription with speaker diarization and HTML output",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Jay Pfaffman",
     url="https://github.com/literatecomputing/transcribe-with-whisper",
