4bd0147e849d3c91f22fd4cc6fab7a57b67aedba
Jay Pfaffman <jay@literatecomputing.com>
2025-09-16 15:11:54 -0500

    refactor looks ok?

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="whisper-transcribe-to-html",
+    name="transcribe-with-whisper",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "pyannote.audio",
         "pydub",
-        "faster-whisper",
         "webvtt-py",
+        "pyannote.audio",
         "huggingface_hub",
+        "torch",            # default CPU version will be installed
+        "faster-whisper",   # will use CPU if torch is CPU-only
     ],
     entry_points={
         "console_scripts": [
-            "whisper-transcribe=whisper_transcribe_to_html.main:main",
+            "transcribe-with-whisper=transcribe_with_whisper.main:main",
         ],
     },
     python_requires=">=3.8",
@@ -21,11 +22,10 @@ setup(
     description="Video transcription with speaker diarization and HTML output",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Your Name",
-    url="https://github.com/literatecomputing/whisper-transcribe-to-html",
+    author="Jay Pfaffman",
+    url="https://github.com/literatecomputing/transcribe-with-whisper",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
 )
-
