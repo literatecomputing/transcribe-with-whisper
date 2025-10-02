f010c1b49a4bffb9d632c514719b608416643743
Jay Pfaffman <jay@literatecomputing.com>
2025-10-02 10:50:15 -0500

    update version to 0.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="transcribe-with-whisper",
-    version="0.5.0",
+    version="0.6.0",
     packages=find_packages(),
     install_requires=[
         "pydub==0.25.1",
