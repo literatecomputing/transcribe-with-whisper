4744c31781173d706d136554c0dffb1381fc2c03
Jay Pfaffman <jay@literatecomputing.com>
2025-10-04 14:07:50 -0500

    release 0.6.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="transcribe-with-whisper",
-    version="0.6.7",
+    version="0.6.8",
     packages=find_packages(),
     install_requires=[
         "pydub==0.25.1",
