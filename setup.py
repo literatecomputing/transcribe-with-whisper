e9dff9d77f080b9dd5528b06eb81a7a30606345e
Jay Pfaffman <jay@literatecomputing.com>
2025-10-30 16:08:57 -0500

    chore(release): bump version to 0.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="transcribe-with-whisper",
-    version="0.8.0",
+    version="0.8.1",
     packages=find_packages(),
     install_requires=[
         "pydub==0.25.1",
